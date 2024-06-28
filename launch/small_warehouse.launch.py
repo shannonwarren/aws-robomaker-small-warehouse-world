11a126e1f66ce9077ac9e2b34f854d1acba05720
Shannon Govekar <shannon@vicirobotics.com>
2024-06-28 11:29:58 -0700

    Add Vici configs

--- launch/small_warehouse.launch.py
+++ launch/small_warehouse.launch.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import os
-
+import launch_ros
 from ament_index_python.packages import get_package_share_directory
 
 import launch
@@ -21,20 +21,26 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration, PythonExpression, Command
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     # Get the launch directory
     aws_small_warehouse_dir = get_package_share_directory('aws_robomaker_small_warehouse_world')
+    felix_navigation_dir = get_package_share_directory('felix_navigation')
+    felix_description_dir = get_package_share_directory('felix_description')
     gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+    urdf_model = LaunchConfiguration('urdf_model')
     
     # Launch configuration variables specific to simulation
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_simulator = LaunchConfiguration('use_simulator')
     headless = LaunchConfiguration('headless')
     world = LaunchConfiguration('world')
+    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
@@ -48,8 +54,14 @@ def generate_launch_description():
 
     declare_world_cmd = DeclareLaunchArgument(
         'world',
-        default_value=os.path.join(aws_small_warehouse_dir, 'worlds', 'small_warehouse', 'small_warehouse.world'),
+        default_value=os.path.join(aws_small_warehouse_dir, 'worlds', 'small_warehouse', 'vici_mart.world'),
         description='Full path to world model file to load')
+
+    declare_model_cmd = DeclareLaunchArgument(
+        'urdf_model',
+        default_value=os.path.join(felix_description_dir, 'urdf', 'robots', 'mecanum.urdf.xacro'),
+        description='Full path to world model file to load')
+
 
     # Specify the actions
     start_gazebo_server_cmd = launch.actions.IncludeLaunchDescription(
@@ -63,6 +75,20 @@ def generate_launch_description():
         condition=IfCondition(PythonExpression(['not ', headless]))
     )
 
+    spawn_entity_cmd = launch_ros.actions.Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'p4_amr', '-topic', 'robot_description',
+                   '-z', '0.0', '-x', '-2', '-y', '5'],
+        output='screen'
+    )
+
+    start_robot_state_publisher_cmd = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', urdf_model])}])
+    
+
     # Create the launch description and populate
     ld = LaunchDescription()
 
@@ -70,9 +96,12 @@ def generate_launch_description():
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_simulator_cmd)
     ld.add_action(declare_world_cmd)
+    ld.add_action(declare_model_cmd)
+    # ld.add_action(spaw    n_entity_cmd)
 
     # Add any conditioned actions
     ld.add_action(start_gazebo_server_cmd)
     ld.add_action(start_gazebo_client_cmd)
+    ld.add_action(start_robot_state_publisher_cmd)
 
     return ld
